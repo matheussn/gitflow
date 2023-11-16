d2e92d189a9b3f87adcdfe1490f1f1ef8f71a833
Matheus Neto <matheusneto23@gmail.com>
2023-11-16 11:54:10 -0300

    testing

--- .github/workflows/compare_bundles.sh
+++ .github/workflows/compare_bundles.sh
@@ -1,1 +1,10 @@
-printenv
+# printenv
+
+# GITHUB_TRIGGERING_ACTOR --> Quem criou o PR
+# GITHUB_BASE_REF --> Base branch
+
+echo $GITHUB_TRIGGERING_ACTOR
+echo $GITHUB_BASE_REF
+
+# git checkout $GITHUB_BASE_REF
+# git pull origin $GITHUB_BASE_REF
