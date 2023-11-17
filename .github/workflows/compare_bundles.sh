1b8813054d7b8ab17c8682b8208145e52e50f140
Matheus Neto <matheusneto23@gmail.com>
2023-11-17 16:24:45 -0300

    adding test workflow

--- .github/workflows/compare_bundles.sh
+++ .github/workflows/compare_bundles.sh
@@ -1,5 +1,5 @@
-echo "$SERVICE_NAME=olá" >> $GITHUB_OUTPUT
-echo "$SERVICE_NAME=olá" >> $RESULTS
-RESULTS="$SERVICE_NAME=olá"
+# echo "$SERVICE_NAME=olá" >> $GITHUB_OUTPUT
+# echo "$SERVICE_NAME=olá" >> $RESULTS
+# RESULTS="$SERVICE_NAME=olá"
 
 printenv
